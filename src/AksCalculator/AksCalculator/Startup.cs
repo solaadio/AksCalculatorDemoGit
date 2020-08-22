7856c2d79cfe080aad053bcdc15818bd52d8414e
OLUSOLA ADIO <61968373+olusola-adio@users.noreply.github.com>
2020-08-22 01:42:19 +0100

    Small change (#4)
    * small change
    
    * small change
    
    * change
    
    * changed email
    
    * agent-pool
    
    * small
    
    * pr
    
    * pr
    
    * pr
    
    * pr
    
    * pr
    
    * win
    
    * token
    
    * pr
    
    * pr
    
    * token
    
    * docker
    
    * docker
    
    * token
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * DOCKER
    
    * dotnet
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * supervisor
    
    * supervisor
    
    * supervisor
    
    * supervisor
    
    * pr request
    
    * install powershell
    
    * rem ps
    
    * add ps
    
    * sdd
    
    * ps
    
    * remove ps
    
    * 3.1
    
    * 3.1
    
    * core
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * upgrade to 3.1
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * docker
    
    * progress
    
    * docker
    
    * docker
    
    * docker
    
    * no terra
    
    * akscalc docker
    
    * block 3
    
    * added sudo
    
    * no sudo
    
    * full agent
    
    * docker
    
    * docker
    
    * volme
    
    * fast
    
    * mount
    
    * pwsh
    
    * replica 2
    
    * repl 1
    
    * replica
    
    * use new pool
    
    * pool
    
    * pr
    
    * pool
    
    * pool
    
    * pr
    
    * pool
    
    * build
    
    * mem
    
    * docker
    
    * pods
    
    * cleanup

--- src/AksCalculator/AksCalculator/Startup.cs
+++ src/AksCalculator/AksCalculator/Startup.cs
@@ -1,8 +1,12 @@
+
+
+
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace AksCalculator
 {
@@ -18,34 +22,38 @@ namespace AksCalculator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+
+
             services.AddMvc();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
                     Version = "v1",
-                    Title = "AksCalculator API",
-                    Description = "Implementation of AksCalculator in ASP.NET Core Web API deployed on to AKS",
-                    TermsOfService = "None",
-                    Contact = new Contact
+                    Title = "VAT Toolbox API",
+                    Description = "VATToolBox ASP.NET Core Web API deployed to AKS",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
                     {
                         Name = "Olusola Adio",
-                        Email = "solaadio@yahoo.com",
-                        Url = "https://twitter.com/iLogion"
+                        Email = "orders@logion.co.uk",
+                        Url = new Uri("https://twitter.com/iLogion"),
                     },
-                    License = new License
+                    License = new OpenApiLicense
                     {
                         Name = "Use under LICX",
-                        Url = "https://twitter.com/iLogion"
+                        Url = new Uri("https://example.com/license"),
                     }
                 });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -53,7 +61,10 @@ namespace AksCalculator
             }
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+            app.UseSwagger(c =>
+            {
+                // c.SerializeAsV2 = true;
+            });
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
@@ -62,7 +73,12 @@ namespace AksCalculator
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "VATToolBox ASP.NET Core Web API deployed to AKS");
                 c.RoutePrefix = string.Empty;
             });
-            app.UseMvc();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
